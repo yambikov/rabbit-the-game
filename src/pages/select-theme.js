f729e51cf3747b13d6130a15da8c055de645f661
yambikov <yambikov@yandex.ru>
2024-01-14 19:56:29 +0300

    02 random theme ok

--- src/pages/select-theme.js
+++ src/pages/select-theme.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import themes from '../components/themesData';
 
-function SelectTheme({ numberOfPlayers }) {
+function ChooseTheme({ numberOfPlayers }) {
+  const [selectedTheme, setSelectedTheme] = useState(null);
+
+  // Используйте useEffect для выбора случайной темы при монтировании компонента
+  useEffect(() => {
+    handleRandomThemeClick();
+  }, []); // Пустой массив зависимостей означает, что эффект будет выполнен только при монтировании
+
   const handleBackClick = () => {
     // Вернуть пользователя к выбору количества игроков
-    window.location.href = '/play';
+    window.location.href = '/select-players';
   };
 
   const handleChooseThemeClick = () => {
-    // Действия при нажатии на кнопку "Выбрать тему"
-    // Возможно, вы хотите реализовать логику выбора темы здесь
+    // Возможно, вы хотите обработать выбранную тему здесь
+    console.log("Нажата кнопка Выбрать тему", selectedTheme);
   };
 
   const handleRandomThemeClick = () => {
-    // Действия при нажатии на кнопку "Случайная тема"
-    // Возможно, вы хотите реализовать логику выбора случайной темы здесь
+    console.log("Нажата кнопка Случайная тема либо обновлена страница");
+    // Выбрать случайную тему
+    const allThemes = Object.keys(themes);
+    console.log("Все темы:", allThemes);
+    const randomThemeType = allThemes[Math.floor(Math.random() * allThemes.length)];
+    console.log("Выбранный тип темы:", randomThemeType);
+    // const themeOptions = themes[randomThemeType];
+    // const randomTheme = themeOptions[Math.floor(Math.random() * themeOptions.length)];
+
+    // Установить выбранную тему в состояние
+    setSelectedTheme(randomThemeType);
   };
 
   const handleStartGameClick = () => {
     // Действия при нажатии на кнопку "Начать игру"
     // Возможно, вы хотите перейти к игровому компоненту с выбранной темой
   };
+
+  
 
   return (
     <div>
@@ -28,8 +47,13 @@ function SelectTheme({ numberOfPlayers }) {
       <button onClick={handleChooseThemeClick}>Выбрать тему</button>
       <button onClick={handleRandomThemeClick}>Случайная тема</button>
       <button onClick={handleStartGameClick}>Начать игру</button>
+
+      <div>
+        <h3>Выбранная тема:</h3>
+        <p>{selectedTheme}</p>
+      </div>
     </div>
   );
 }
 
-export default SelectTheme;
+export default ChooseTheme;
